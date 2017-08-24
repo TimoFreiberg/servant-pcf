3f8a09ef64efe42c76a7ef115bcd9d62214c4f86
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2017-08-24 12:43:10 +0200

    Remove db stuff, make it minimal

--- app/Main.hs
+++ app/Main.hs
@@ -8,36 +8,45 @@ module Main
   ( main
   ) where
 
-import qualified Control.Monad.Logger as Log
-import Data.Pool (Pool)
-import Database.Persist.Sqlite (SqlBackend, createSqlitePool)
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Network.Wai.Logger as Log
 import qualified Servant as Servant
-import Servant ((:<|>)((:<|>)))
+import Servant ((:>),(:<|>)((:<|>)))
+import System.Environment(getEnv)
+import Control.Monad.Catch(throwM)
 
-import Api
 import Protolude
+
+import App
 
 main :: IO ()
 main = app
 
-dbIdentifier :: Text
-dbIdentifier = "db.sqlite3"
+-- dbIdentifier :: Text
+-- dbIdentifier = "db.sqlite3"
+type HealthApi = "health" :> Servant.Get '[ Servant.JSON] Text
+type KillApi = "kill" :> Servant.Get '[Servant.JSON] Text
 
 app :: IO ()
 app = do
-  let port = 8081 :: Int
+  portString <- getEnv "PORT"
+  port <- case readMaybe @Int portString of
+    Just int -> pure int
+    Nothing -> (throwM (ConfigException ("PORT must be a number, was "<> strConv Lenient portString)))
+
   putText $ "starting server on port " <> show port
-  (pool :: Pool SqlBackend) <-
-    Log.runStdoutLoggingT (createSqlitePool dbIdentifier 10)
-  runMigration pool
+  -- (pool :: Pool SqlBackend) <-
+  --   Log.runStdoutLoggingT (createSqlitePool dbIdentifier 10)
+  -- runMigration pool
   Log.withStdoutLogger $ \logger -> do
     let settings =
           Warp.setPort port $ Warp.setLogger logger Warp.defaultSettings
     Warp.runSettings
       settings
       (Servant.serve
-         (Proxy @UsersApi)
-         (getAllUsers pool :<|> createUser pool :<|> getUser pool))
+         (Proxy @(HealthApi :<|> KillApi))
+         (getHealth :<|> error "KILLED"))
   -- Warp.run 8081 (Servant.serve (Proxy @GetAllUsers) (getAllUsers conn))
+
+getHealth :: Servant.Handler Text
+getHealth = return "Up"
