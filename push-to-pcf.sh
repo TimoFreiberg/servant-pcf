6ff0995091876548efe389d86784580a69a52173
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2017-08-29 10:31:46 +0200

    Make push-to-pcf.sh more robust with binary path, print command

--- push-to-pcf.sh
+++ push-to-pcf.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 APPNAME=servant-health
-BINARY_PATH=".stack-work/install/x86_64-linux-nopie/lts-9.1/8.0.2/bin/servant-pcf"
+BIN_ROOT=$($HOME/.local/bin/stack path --local-install-root)
+BINARY_PATH="$BIN_ROOT/bin/servant-pcf"
 BUILDPACK_URL="https://github.com/cloudfoundry/binary-buildpack.git"
 MEMORY=100MB
-cf push "$APPNAME" -c "$BINARY_PATH" -b "$BUILDPACK_URL" -m "$MEMORY"
+CMD="cf push $APPNAME -c $BINARY_PATH -b $BUILDPACK_URL -m $MEMORY"
+echo "Running command:"
+echo $CMD
+$CMD
+
+
